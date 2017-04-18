92551491ec9ba160e5c7af813280447cae127d78
Dante G <dante.gray96@gmail.com>
2017-04-18 23:51:35 +0100

    Initial Template gameLogic.h & Board 'object'

--- gameLogic.h
+++ gameLogic.h
@@ -1,12 +1,31 @@
 #ifndef __GAMELOGIC_H__
 #define __GAMELOGIC_H__
 
+#include "chessPieces.h"
 
 #define TOTAL_SQUARES 64
 
+typedef enum turn {WHITE, BLACK} turn;
 
 struct board {
 
+	turn playerColour;
+
+	piece *tiles;
+	
+	turn playerTurn;
+
+	int isWhiteInCheck;
+	int isBlackInCheck;
 }
+typedef struct board board;
+
+void board_setup(board *table);
+
+void update_king_status(chessPiece *king);
+
+int is_my_turn(turn playerTurn)
+
+int is_king_in_check(chessPiece *king);
 
 #endif
