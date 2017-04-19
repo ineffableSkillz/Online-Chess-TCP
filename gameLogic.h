67b3e2e11e816fbeab59a0aded9e59f2d8dd53c7
Dante G <dante.gray96@gmail.com>
2017-04-19 01:01:32 +0100

    Syntactical Errors Resolved

--- gameLogic.h
+++ gameLogic.h
@@ -4,6 +4,7 @@
 #include "chessPieces.h"
 
 #define TOTAL_SQUARES 64
+#define COLUMN 8
 
 typedef enum turn {WHITE, BLACK} turn;
 
@@ -11,20 +12,20 @@ struct board {
 
 	turn playerColour;
 
-	piece *tiles;
+	chessPiece *tiles;
 	
 	turn playerTurn;
 
 	int isWhiteInCheck;
 	int isBlackInCheck;
-}
+};
 typedef struct board board;
 
 void board_setup(board *table);
 
 void update_king_status(chessPiece *king);
 
-int is_my_turn(turn playerTurn)
+int is_my_turn(turn playerTurn);
 
 int is_king_in_check(chessPiece *king);
 
